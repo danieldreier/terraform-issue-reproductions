6c2e82219111d499ea1de1d370f67d563b9176ea
Daniel Dreier <ddreier@hashicorp.com>
2020-05-19 09:58:16 -0700

    24520: crash: when HOME environment variable is unset

---
+++ 24520/main.tf
@@ -0,0 +1,6 @@
+resource "null_resource" "example" {
+
+  provisioner "local-exec" {
+    command = "echo hello world"
+  }
+}
---
+++ 24520/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+unset HOME
+rm -rf .terraform
+terraform init
