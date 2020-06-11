b1c0647288218b1cc6cd648340a2b769b40eb6a6
Daniel Dreier <ddreier@hashicorp.com>
2020-06-11 16:04:47 -0700

    repro for 25221

---
+++ 25221/25221.tf
@@ -0,0 +1,34 @@
+  module "network_isolated" {
+  source        = "../../modules/network_isolated"
+  environment   = var.environment
+  region        = var.region
+  address_space = var.address_space
+  tags          = var.tags
+}
+
+###
+#  module composition
+###
+
+#resource group
+
+module "resource_group" {
+  source       = "../../resources/base/resource_group"
+  service_name = "network-isolated"
+  region       = var.region
+  environment  = var.environment
+  tags         = var.tags
+}
+
+#vnet
+
+module "virtual_network" {
+  source         = "../../resources/network/virtual_network"
+  resource_group = module.resource_group.name
+  region         = module.resource_group.location
+  environment    = var.environment
+  name_prefix    = "vnet-isolated"
+  address_space  = var.address_space
+  tags           = var.tags
+}
+
